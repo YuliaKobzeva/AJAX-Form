193b833b262f075949e4eda83c9dbaa1612fe1b1
Yuliya <lomeyka@gmail.com>
2020-10-29 14:21:58 +0300

    realized post request to the fake server, form.reset(). Next time - setTimeout(?) and css

--- form.js
+++ form.js
@@ -1,1 +1,13 @@
-console.log(1);
+const form = document.querySelector(".form");
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = JSON.stringify(new FormData(form)); /*сериализация данных формы в JSON*/
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'post',
+    body: formData
+  })
+  .then(response => response.text())
+  .catch(error => console.error(error));
+  form.reset();
+
+});
