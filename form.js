0bada4faac38688d2e94fad5db0edcaba0f665fe
Yuliya <lomeyka@gmail.com>
2020-10-29 15:13:47 +0300

    added loader

--- form.js
+++ form.js
@@ -1,4 +1,9 @@
 const form = document.querySelector(".form");
+const contentWrapper = document.querySelector('.form-container');
+const buttonSubmit = document.querySelector('.form__button');
+const loader = document.createElement('div');
+loader.classList.add('lds-dual-ring');
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const formData = JSON.stringify(new FormData(form)); /*сериализация данных формы в JSON*/
@@ -7,7 +12,17 @@ form.addEventListener('submit', (e) => {
     body: formData
   })
   .then(response => response.text())
+  .then(response => {
+    if (response) {
+      setTimeout(() => {
+        console.log("прошло 3 секунды")
+      }, 3000);
+    }
+  })
   .catch(error => console.error(error));
   form.reset();
+  contentWrapper.removeChild(buttonSubmit);
+  contentWrapper.appendChild(loader);
+
 
 });
