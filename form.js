b2fadfd14522e069c34dac73cb3755dc38c2941d
Yuliya <lomeyka@gmail.com>
2020-10-30 09:48:59 +0300

    test completed

--- form.js
+++ form.js
@@ -1,40 +1,34 @@
 const form = document.querySelector(".form");
-const contentWrapper = document.querySelector('.form-container');
-const buttonSubmit = document.querySelector('.form__button');
+const contentWrapper = document.querySelector(".form-container");
+const buttonSubmit = document.querySelector(".form__button");
 
-const loader = document.createElement('div');
-loader.classList.add('lds-dual-ring');
+const loader = document.createElement("div");
+loader.classList.add("lds-dual-ring");
 
-okMessage = document.createElement('div');
-okMessage.classList.add('ok-message');
+okMessage = document.createElement("div");
+okMessage.classList.add("ok-message");
 okMessage.textContent = "Форма успешно отправлена!";
 
 function afterResponse() {
-  contentWrapper.removeChild(loader);
-  contentWrapper.appendChild(okMessage);
-  console.log("прошло 3 секунды")
+	contentWrapper.removeChild(loader);
+	contentWrapper.appendChild(okMessage);
 }
 
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const formData = JSON.stringify(new FormData(form)); /*сериализация данных формы в JSON*/
-  fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'post',
-    body: formData
-  })
-  .then(response => response.text())
-  .then(response => {
-    if (response) {
-      setTimeout(afterResponse, 3000);
-    }
-  })
-  .catch(error => console.error(error));
-  form.reset();
-  contentWrapper.removeChild(buttonSubmit);
-  contentWrapper.appendChild(loader);
-
-
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const formData = JSON.stringify(new FormData(form)); /*сериализация данных формы в JSON*/
+	fetch("https://jsonplaceholder.typicode.com/posts", {
+		method: "post",
+		body: formData,
+	})
+		.then((response) => response.text())
+		.then((response) => {
+      if (response) {
+				setTimeout(afterResponse, 3000);
+			}
+		})
+		.catch((error) => console.error(error));
+	form.reset();
+	contentWrapper.removeChild(buttonSubmit);
+	contentWrapper.appendChild(loader);
 });
-
-/*поля, обязательный для заполнения + статусы ответов*/
